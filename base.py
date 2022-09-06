c53c829036e9b2e2c18d530c7a8f06e8b33ba6ac
Gerarld <gerarld73@gmail.com>
2022-09-05 18:38:47 -0600

    update

--- base.py
+++ base.py
@@ -3,10 +3,11 @@
 class Cuadrado:
     def __init__(self, a, b):
         self.lado_a = a
+        self.lado_c = 2
         self.lado_b = b
 
     def area(self):
-        return self.lado_a * self.lado_b
+        return self.lado_a * self.lado_c
 
     def perimetro_2(self):
         return self.lado_a+self.lado_a+self.lado_a+self.lado_a
