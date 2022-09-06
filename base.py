a0394e301e0f48fdefcadcc5b3cafab8e1c7f7c9
Gerarld <gerarld73@gmail.com>
2022-09-05 18:30:27 -0600

    create func perimetro

--- base.py
+++ base.py
@@ -1,8 +1,9 @@
 #Este es un ejemplo de python para git
 
 class Cuadrado:
-    def __init__(self):
-        pass
+    def __init__(self, a, b):
+        self.lado_a = a
+        self.lado_b = b
 
     def perimetro(self):
-        pass
+        return self.lado_a*4
